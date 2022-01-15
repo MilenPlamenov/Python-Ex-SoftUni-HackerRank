642f61a775206427e3f0cb6205181baac396f36c
Милен Георгиев <77685128+MilenPlamenov@users.noreply.github.com>
2022-01-15 22:45:45 +0200

    Delete gui_shop_by_milen directory

--- gui_shop_by_milen/file_executor.py
+++
@@ -1,8 +0,0 @@
-from gui_shop_by_milen.authentication import render_main_enter_screen
-from gui_shop_by_milen.canvas import tk
-#from gui_shop.products import render_products
-
-if __name__ == "__main__":
-    render_main_enter_screen()
-    tk.withdraw()
-    tk.mainloop()
--- gui_shop_by_milen/helpers.py
+++
@@ -1,6 +0,0 @@
-from gui_shop_by_milen.canvas import tk
-
-
-def clean_screen():
-    for el in tk.grid_slaves():
-        el.destroy()
